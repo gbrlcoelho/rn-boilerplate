a61ba36f5766b87044b52e1b0655949c18b5dae5
gbrlcoelho <coelhogabriel67@gmail.com>
2023-11-20 15:44:39 -0300

    feat(test): setup test files

---
+++ template/src/test/jestSetup.ts
@@ -0,0 +1,18 @@
+export {};
+
+// @ts-ignore
+import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock';
+jest.mock('react-native-safe-area-context', () => ({
+  ...mockSafeAreaContext,
+  useSafeAreaInsets: jest.fn(mockSafeAreaContext.useSafeAreaInsets),
+}));
+
+jest.mock('@react-navigation/native', () => {
+  const originalModule = jest.requireActual('@react-navigation/native');
+  return {
+    ...originalModule,
+    useNavigation: () => ({
+      navigate: jest.fn(),
+    }),
+  };
+});
---
+++ template/src/test/test-utils.tsx
@@ -0,0 +1,69 @@
+import React, {ReactElement} from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {ThemeProvider} from '@shopify/restyle';
+import {
+  QueryClient,
+  QueryClientConfig,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import {
+  RenderHookOptions,
+  RenderOptions,
+  render,
+  renderHook,
+} from '@testing-library/react-native';
+
+import {theme} from '@theme';
+
+const queryClientConfig: QueryClientConfig = {
+  defaultOptions: {
+    queries: {
+      retry: false,
+      cacheTime: Infinity,
+    },
+    mutations: {
+      retry: false,
+      cacheTime: Infinity,
+    },
+  },
+  logger: {
+    log: console.log,
+    warn: console.warn,
+    // @ts-ignore
+    error: process.env.NODE_ENV === 'test' ? () => {} : console.error,
+  },
+};
+
+export const wrapAllProviders = () => {
+  const queryClient = new QueryClient(queryClientConfig);
+
+  return ({children}: {children: React.ReactNode}) => {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <NavigationContainer>{children}</NavigationContainer>
+        </ThemeProvider>
+      </QueryClientProvider>
+    );
+  };
+};
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>,
+) => render(ui, {wrapper: wrapAllProviders(), ...options});
+
+const customRenderHook = <Result, Props>(
+  renderCallback: (props: Props) => Result,
+  options?: Omit<RenderHookOptions<Props>, 'wrapper'>,
+) => {
+  return renderHook(renderCallback, {
+    wrapper: wrapAllProviders(),
+    ...options,
+  });
+};
+
+export * from '@testing-library/react-native';
+
+export {customRender as render, customRenderHook as renderHook};
