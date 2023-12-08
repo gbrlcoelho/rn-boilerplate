4b38982f19a9fab711514c73c44294a78589306c
gbrlcoelho <coelhogabriel67@gmail.com>
2023-12-08 13:50:41 -0300

    feat: added logs to post init script

--- script.js
+++ script.js
@@ -35,13 +35,17 @@ const installDependencies = () => {
     "husky",
   ];
 
-  console.log("Installing dependencies...");
-  execSync(`yarn add ${dependencies.join(" ")}`, { stdio: "inherit" });
-  console.log("Dependencies installed successfully.");
+  console.log(
+    "@gbrlcoelho/react-native-boilerplate initialized with success! 🚀\n"
+  );
 
-  console.log("Installing dev dependencies...");
+  console.log("Installing dependencies... 🛠️\n");
+  execSync(`yarn add ${dependencies.join(" ")}`, { stdio: "inherit" });
+  console.log("Dependencies installed successfully. 🚀\n");
+
+  console.log("Installing dev dependencies... 🛠️\n");
   execSync(`yarn add ${devDependencies.join(" ")} -D`, { stdio: "inherit" });
-  console.log("Dev dependencies installed successfully.");
+  console.log("Dev dependencies installed successfully.🚀\n");
 };
 
 const main = () => {
@@ -54,10 +58,21 @@ new Promise((resolve) => {
   resolve();
 })
   .then(() => {
-    console.log("Post init script executed successfully.");
+    console.log(
+      "- 🎉  Congrats! Your project is ready with @gbrlcoelho/react-native-boilerplate! 🎉\n"
+    );
+
+    console.log(
+      "- 📚 If you need to read more about this boilerplate : https://github.com/gbrlcoelho/rn-boilerplate/blob/master/README.md\n"
+    );
+    console.log(
+      "- 🤕 If you have some troubles : https://github.com/gbrlcoelho/rn-boilerplate/issues\n"
+    );
+    console.log(
+      "- ⭐ If you love this boilerplate, give us a star, you will be a ray of sunshine in our lives :) https://github.com/gbrlcoelho/rn-boilerplate\n"
+    );
   })
   .catch((error) => {
-    throw new Error(
-      `An error occurred while executing the post init script: ${error}`
-    );
+    console.error(`🚨 An error occurred with post init script: ${error}`);
+    throw new Error();
   });
